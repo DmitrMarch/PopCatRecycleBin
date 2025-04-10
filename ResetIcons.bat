3202a42e8a325f5d29e0b9525f8800e85cc0c585
Дмитрий Марченко <mdi500100@gmail.com>
2025-04-10 23:26:01 +0300

    init git

---
+++ ResetIcons.bat
@@ -0,0 +1,9 @@
+:: возвращение дефолтной корзины
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon"^
+ /ve /t REG_EXPAND_SZ /d "%SystemRoot%\\System32\\imageres.dll","-55" /f
+
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon"^
+ /v "empty" /t REG_EXPAND_SZ /d "%SystemRoot%\\System32\\imageres.dll","-55" /f
+
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon"^
+ /v "full" /t REG_EXPAND_SZ /d "%SystemRoot%\\System32\\imageres.dll","-54" /f
---
+++ SetIcons.bat
@@ -0,0 +1,11 @@
+:: изменение иконки текущей корзины
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon"^
+ /ve /t REG_SZ /d "%CD%\PopCatOpen.ico,0" /f
+
+:: изменение иконки пустой корзины
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon"^
+ /v "empty" /t REG_SZ /d "%CD%\PopCatOpen.ico,0" /f
+
+:: изменение иконки заполненной корзины
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon"^
+ /v "full" /t REG_SZ /d "%CD%\PopCatClosed.ico,0" /f
